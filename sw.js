26cb6e44ff3ad467fc4f88c1556c50bb4af9eeec
TheJetDev <thejetdev.contact@gmail.com>
2026-02-28 16:44:36 +0900

    Update service worker cache version to v2.8.8

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js (Free Version)
-const CACHE_NAME = 'simple-sheet-v2.8.7'; // 
+const CACHE_NAME = 'simple-sheet-v2.8.8'; // 
 
 const urlsToCache = [
   'index.html',
@@ -63,6 +63,7 @@ self.addEventListener('fetch', event => {
     );
   }
 });
+
 
 
 
